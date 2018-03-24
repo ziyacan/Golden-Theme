eda850486cf5d82c28c08e28a18aec40c27f8aa6
ziya <ziyacanistanbullu@gmail.com>
2018-03-24 18:11:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/*START SCROOL COLOR*/
+jQuery(document).ready(function($){
+    $(window).scroll(function(){
+        var scrollPosition = $(window).scrollTop(),
+        navbar= $('.fixed-top');
+        if (scrollPosition > 400){
+            navbar.addClass('change-fix-top');
+        } else {
+            navbar.removeClass('change-fix-top');
+        }
+    });
+});
+/*END SCROOL COLOR*/
+
+
+/*START SMOOTH SCROLL*/
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+/*END SMOOTH SCROLL*/
